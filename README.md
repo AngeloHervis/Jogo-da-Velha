021da0c61910fca5ad54e3c616db1b61e502f51f
Angelo Hervis <angelo.hervis@gmail.com>
2024-02-20 21:35:50 -0300

    att

---
+++ README.md
@@ -0,0 +1,33 @@
+# Jogo da Velha
+
+<div style="display:flex; align-items:center; justify-content:center; margin-bottom:20px">
+<img src="img/projeto.png" >
+</div>
+
+## Descrição
+O Jogo da Velha é uma implementação simples e divertida do clássico jogo da velha para jogar no navegador. Este projeto foi desenvolvido como parte do meu aprendizado em desenvolvimento web.
+
+## Funcionalidades Principais
+- Interface intuitiva e fácil de usar.
+- Possibilidade de jogar contra outro jogador.
+- Design responsivo para se adaptar a diferentes dispositivos.
+
+## Tecnologias Utilizadas
+- HTML5
+- CSS3
+- JavaScript
+- Git (para controle de versão)
+
+## Como Utilizar
+1. Clone o repositório (`git clone https://github.com/seu-usuario/jogo-da-velha.git`).
+2. Abra o arquivo `index.html` em seu navegador web.
+3. Insira os nomes dos jogadores nos campos de entrada.
+4. Clique no botão "Começar!" para iniciar o jogo.
+5. Clique em uma célula vazia do tabuleiro para fazer sua jogada.
+6. O primeiro jogador a conseguir três símbolos iguais em linha, coluna ou diagonal vence.
+7. Divirta-se jogando!
+
+## Autor
+Angelo Hervis
+- GitHub: [AngeloHervis](https://github.com/AngeloHervis)
+- LinkedIn: [Angelo Hervis](https://www.linkedin.com/in/angelo-hervis/))
--- script.js
+++ script.js
@@ -1,83 +1,82 @@
-const boardRegions = document.querySelectorAll('#gameBoard span')
-let vBoard = []
-let turnPlayer = ''
+const boardRegions = document.querySelectorAll('#gameBoard .cell');
+let vBoard = [];
+let turnPlayer = '';
 
-function updateTitle () {
-    const playerInput = document.getElementById(turnPlayer)
-    document.getElementById('turnPlayer').innerText = playerInput.value
+function updateTitle() {
+    const playerInput = document.getElementById(turnPlayer);
+    document.getElementById('turnPlayer').innerText = playerInput.value;
 }
 
-function initializeGame () {
-    vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
-    turnPlayer = 'player1'
-    document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
-    updateTitle ()
-    boardRegions.forEach(function (element) {
-        element.classList.remove('win')
-        element.innerText = ''
-        element.classList.add('cursor-pointer')
-        element.addEventListener ('click', handleBoardClick)
-    })
+function initializeGame() {
+    vBoard = [['', '', ''], ['', '', ''], ['', '', '']];
+    turnPlayer = 'player1';
+    document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>';
+    updateTitle();
+    boardRegions.forEach(function(element) {
+        element.classList.remove('win');
+        element.innerText = '';
+        element.classList.add('cursor-pointer');
+        element.addEventListener('click', handleBoardClick);
+    });
 }
 
-function getWinRegions () {
-    const winRegions = []
-    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
-    winRegions.push("0.0", "0.1", "0.2")
-  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
-    winRegions.push("1.0", "1.1", "1.2")
-  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
-    winRegions.push("2.0", "2.1", "2.2")
-  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
-    winRegions.push("0.0", "1.0", "2.0")
-  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
-    winRegions.push("0.1", "1.1", "2.1")
-  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
-    winRegions.push("0.2", "1.2", "2.2")
-  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
-    winRegions.push("0.0", "1.1", "2.2")
-  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
-    winRegions.push("0.2", "1.1", "2.0")
-  return winRegions
-
+function getWinRegions() {
+    const winRegions = [];
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2]) winRegions.push("0.0", "0.1", "0.2");
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2]) winRegions.push("1.0", "1.1", "1.2");
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2]) winRegions.push("2.0", "2.1", "2.2");
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0]) winRegions.push("0.0", "1.0", "2.0");
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1]) winRegions.push("0.1", "1.1", "2.1");
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2]) winRegions.push("0.2", "1.2", "2.2");
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2]) winRegions.push("0.0", "1.1", "2.2");
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0]) winRegions.push("0.2", "1.1", "2.0");
+    return winRegions;
 }
 
-function disableRegion (element) {
-    element.classList.remove('cursor-pointer')
-    element.removeEventListener('click', handleBoardClick)
+function disableRegion(element) {
+    element.classList.remove('cursor-pointer');
+    element.removeEventListener('click', handleBoardClick);
 }
 
-function handleWin (regions) {
+function handleWin(regions) {
     regions.forEach(function(region) {
-        document.querySelector('[data-region="' + region +'"]').classList.add('win')
-        const playerName = document.getElementById(turnPlayer).value
-        document.querySelector('h2').innerHTML = playerName + ' venceu!'
-    })
+        document.querySelector('[data-region="' + region + '"]').classList.add('win');
+        const playerName = document.getElementById(turnPlayer).value;
+        document.querySelector('h2').innerHTML = playerName + ' venceu!';
+    });
+    // Desabilita todas as regiões após a vitória
+    boardRegions.forEach(function(element) {
+        element.removeEventListener('click', handleBoardClick);
+    });
 }
 
-function handleBoardClick (ev) {
-    const span = ev.currentTarget
-    const region = span.dataset.region
-    const rowColumnPair = region.split('.')
-    const row = rowColumnPair[0]
-    const column = rowColumnPair[1]
+function handleBoardClick(ev) {
+    const span = ev.currentTarget;
+    const region = span.dataset.region;
+    const rowColumnPair = region.split('.');
+    const row = rowColumnPair[0];
+    const column = rowColumnPair[1];
+    if (vBoard[row][column] !== '') {
+        // Se a célula já estiver preenchida, retorna sem fazer nada
+        return;
+    }
     if (turnPlayer === 'player1') {
-        span.innerText = 'X'
-        vBoard[row] [column] = 'X'
+        span.innerText = 'X';
+        vBoard[row][column] = 'X';
     } else {
-        span.innerText = 'O'
-        vBoard[row] [column] = 'O'
+        span.innerText = 'O';
+        vBoard[row][column] = 'O';
     }
-    disableRegion(span)
-    let winRegions = getWinRegions()
+    disableRegion(span);
+    let winRegions = getWinRegions();
     if (winRegions.length > 0) {
-        handleWin(winRegions)
+        handleWin(winRegions);
     } else if (vBoard.flat().includes('')) {
-        turnPlayer = turnPlayer === 'player1' ? 'player2': 'player1'
-        updateTitle()
+        turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1';
+        updateTitle();
     } else {
-        document.querySelector('h2').innerHTML = 'Empate!'
+        document.querySelector('h2').innerHTML = 'Empate!';
     }
 }
 
-document.getElementById('start').addEventListener('click', initializeGame)
+document.getElementById('start').addEventListener('click', initializeGame);
